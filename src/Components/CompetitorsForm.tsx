9acfb1f83efeb6d2978de7867e731e8f068ad7c4
jh-zzzzz <jacobheinegard@hotmail.com>
2024-05-01 14:45:46 +0200

    use fetcher function

--- src/Components/CompetitorsForm.tsx
+++ src/Components/CompetitorsForm.tsx
@@ -1,5 +1,5 @@
 import { FormEvent } from "react";
-import { Opponent } from "../App";
+import { getMatchesForCompetitor } from "../http";
 
 type CompetitorsFormProps = {
   setCompetitor1: Function;
@@ -23,51 +23,10 @@ export const CompetitorsForm = ({
     if (!competitor1.value || !competitor2.value) {
       // TODO: disallow empty id
     }
-    let oppsFor1: Opponent[] = [];
-    fetch(
-      "https://smoothcomp.com/en/profile/"
-        .concat(competitor1.value)
-        .concat("/events"),
-    )
-      .then((resp) => resp.json())
-      .then((json) => {
-        for (let i in json.data) {
-          for (let j in json.data[i].registrations) {
-            for (let k in json.data[i].registrations[j].matches) {
-              oppsFor1.push({
-                name: json.data[i].registrations[j].matches[k].opponents[0]
-                  .name,
-                id: json.data[i].registrations[j].matches[k].opponents[0].id,
-                isWinner: json.data[i].registrations[j].matches[k].is_winner,
-              });
-            }
-          }
-        }
-        setCompetitor1({ name: "", id: competitor1.value, opps: oppsFor1 });
-      });
-
-    let oppsFor2: Opponent[] = [];
-    fetch(
-      "https://smoothcomp.com/en/profile/"
-        .concat(competitor2.value)
-        .concat("/events"),
-    )
-      .then((resp) => resp.json())
-      .then((json) => {
-        for (let i in json.data) {
-          for (let j in json.data[i].registrations) {
-            for (let k in json.data[i].registrations[j].matches) {
-              oppsFor2.push({
-                name: json.data[i].registrations[j].matches[k].opponents[0]
-                  .name,
-                id: json.data[i].registrations[j].matches[k].opponents[0].id,
-                isWinner: json.data[i].registrations[j].matches[k].is_winner,
-              });
-            }
-          }
-        }
-        setCompetitor2({ name: "", id: competitor2.value, opps: oppsFor1 });
-      });
+    getMatchesForCompetitor(competitor1.value)
+      .then(opps => setCompetitor1({ name: "", id: competitor1.value, opps: opps }));
+    getMatchesForCompetitor(competitor2.value)
+      .then(opps => setCompetitor2({ name: "", id: competitor2.value, opps: opps }));
   };
 
   return (
