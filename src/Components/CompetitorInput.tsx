466b4e5838bcd134f9078c41699f79289d6ad99e
jh-zzzzz <jacobheinegard@hotmail.com>
2024-05-20 22:49:28 +0200

    check competitor name

--- src/Components/CompetitorInput.tsx
+++ src/Components/CompetitorInput.tsx
@@ -1,8 +1,26 @@
-export const CompetitorInput = ({num}: {num: number}) => {
+import { MouseEventHandler, useRef, useState } from "react";
+import { getNameForCompetitor } from "../http";
+
+
+
+export const CompetitorInput = ({num}: {num: number}, {name}: {name: string}) => {
+    const [competitorName, setCompetitorName] = useState<string>("");
+    const competitorIdInput = useRef<HTMLInputElement>(null);
+
+    const getName: MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.preventDefault();
+        if (competitorIdInput) getNameForCompetitor(competitorIdInput.current!.value)
+            .then(name => setCompetitorName(name));
+    }
+
     return (
-        <>
+        <div>
             <label htmlFor={`competitor${num}`}>competitor {num}</label>
-            <input type="text" id={`competitor${num}`} />
-        </>
+            <input ref={competitorIdInput} type="text" id={`competitor${num}`} />
+            <button type="button" onClick={getName}>Check</button>
+            <br />
+            <label htmlFor={`name${num}`}>Name: </label>
+            <input type="text" id={`name${num}`} disabled value={competitorName} />
+        </div>
     );
 }
