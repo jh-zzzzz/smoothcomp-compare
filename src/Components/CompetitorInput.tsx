480290419b940ca7b156a576ded4e0cfcca6fda7
jh-zzzzz <jacobheinegard@hotmail.com>
2024-05-23 21:23:38 +0200

    useFieldArray for dymic inputs

--- src/Components/CompetitorInput.tsx
+++ src/Components/CompetitorInput.tsx
@@ -3,7 +3,7 @@ import { getNameForCompetitor } from "../http";
 
 
 
-export const CompetitorInput = ({ num, state, removeInput, name }: { num: number; state: { inputs: string[], setter: Dispatch<React.SetStateAction<string[]>> }; removeInput: Function; name?: string }) => {
+export const CompetitorInput = ({ num, removeInput, name }: { num: number; removeInput: Function; name?: string }) => {
     const competitorIdInput = useRef<HTMLInputElement>(null);
 
     const getName = (e: MouseEvent<HTMLButtonElement>) => {
@@ -12,22 +12,22 @@ export const CompetitorInput = ({ num, state, removeInput, name }: { num: number
             .then(n => name = n);
     }
 
-    const handleOnChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
-        e.preventDefault();
-        let data = [...state.inputs];
-        data[index] = e.target.value;
-        state.setter(data);
-    }
+    // const handleOnChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+    //     e.preventDefault();
+    //     let data = [...state.inputs];
+    //     data[index] = e.target.value;
+    //     state.setter(data);
+    // }
 
     return (
         <div>
             <label htmlFor={`competitor${num}`}>competitor {num + 1}</label>
-            <input ref={competitorIdInput} type="text" id={`competitor${num}`} onChange={e => handleOnChange(e, num)} />
+            <input ref={competitorIdInput} type="text" id={`competitor${num}`} />
             <button type="button" onClick={getName}>Check</button>
             <br />
             <label htmlFor={`name${num}`}>Name: </label>
             <input type="text" id={`name${num}`} disabled value={name} />
-            <button disabled={state.inputs.length <= 2} onClick={e => removeInput(e, num)}>Remove input {num + 1}</button>
+            <button onClick={() => removeInput(num)}>Remove input {num + 1}</button>
         </div>
     );
 }
--- src/Components/CompetitorsForm.tsx
+++ src/Components/CompetitorsForm.tsx
@@ -2,6 +2,7 @@ import { Dispatch, FormEvent, MouseEvent, SetStateAction, useState } from "react
 import { getCompetitorInfo, } from "../http";
 import { CompetitorInput } from "./CompetitorInput";
 import { CompetitorInfo } from "../types";
+import { useFieldArray, useForm } from "react-hook-form";
 
 type CompetitorsFormEvent = FormEvent<HTMLFormElement> & {
   target: {
@@ -13,40 +14,49 @@ type CompetitorsFormEvent = FormEvent<HTMLFormElement> & {
 export const CompetitorsForm = ({
   setCompetitors
 }: { setCompetitors: Dispatch<SetStateAction<CompetitorInfo[] | undefined>> }) => {
-  const [competitorNames, setCompetitorsNames] = useState<string[]>([]);
-  const [inputs, setInputs] = useState<string[]>(["", ""]);
-
-  const handleFormSubmit = (e: CompetitorsFormEvent) => {
-    e.preventDefault();
-    Promise.all(Array.from(inputs.map(input => getCompetitorInfo(input))))
-      .then(info => {
-        setCompetitors(info);
-        setCompetitorsNames(info.map(i => i.name));
-      });
-  };
-
-  const addInput = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setInputs([...inputs, ""]);
-  };
-
-  const removeInput = (e: MouseEvent<HTMLButtonElement>, index: number) => {
-    e.preventDefault();
-    if (inputs.length > 2) {
-      let data = [...inputs];
-      data.splice(index, 1);
-      setInputs(data);
+  // const [competitorNames, setCompetitorsNames] = useState<string[]>([]);
+  // const [inputs, setInputs] = useState<string[]>(["", ""]);
+  const { control } = useForm({
+    defaultValues: {
+      inputs: [
+        { input: "" },
+        { input: "" }
+      ]
     }
-  };
+  });
+  const { fields, remove, append } = useFieldArray({ control, name: "inputs" });
+
+  // const handleFormSubmit = (e: CompetitorsFormEvent) => {
+  //   e.preventDefault();
+  //   Promise.all(Array.from(inputs.map(input => getCompetitorInfo(input))))
+  //     .then(info => {
+  //       setCompetitors(info);
+  //       setCompetitorsNames(info.map(i => i.name));
+  //     });
+  // };
+
+  // const addInput = (e: MouseEvent<HTMLButtonElement>) => {
+  //   e.preventDefault();
+  //   setInputs([...inputs, ""]);
+  // };
+
+  // const removeInput = (e: MouseEvent<HTMLButtonElement>, index: number) => {
+  //   e.preventDefault();
+  //   if (inputs.length > 2) {
+  //     let data = [...inputs];
+  //     data.splice(index, 1);
+  //     setInputs(data);
+  //   }
+  // };
 
   return (
     <>
-      <form onSubmit={handleFormSubmit}>
-        {inputs.map((_input, index) => (
-          <CompetitorInput key={index} num={index} state={{ inputs: inputs, setter: setInputs }} removeInput={removeInput} name={competitorNames[index]} />
+      <form onSubmit={/* handleFormSubmit */ () => {}}>
+        {fields.map((input, index) => (
+          <CompetitorInput key={input.id} num={index} /* state={{ inputs: inputs, setter: setInputs }} */ removeInput={remove} /* name={competitorNames[index]} */ />
         ))}
         <input type="submit" value="Compare!" />
-        <button onClick={addInput}>Add input</button>
+        <button onClick={(e) => {e.preventDefault();append({ input: "" })}}>Add input</button>
       </form>
     </>
   );
